6cea9993b793dde807dd0504522e36c377819781
I8087 <pythonic86@gmail.com>
2015-06-05 16:47:08 -0400

    Fixed a caller issue.
    The caller now saves CX.

--- src/os_load_file.asm
+++ src/os_load_file.asm
@@ -1,14 +1,19 @@
 _os_load_file:
     push bp
     mov bp, sp
-    
+    sub sp, 2
+
+    mov word [ep-2], cx
+
     mov ax, word [bp+4]
     mov cx, word [bp+6]
-    
+
     call os_load_file
-    
+
+    mov [ep-2], word cx
+
     mov ax, bx
-    
+
+    mov sp, bp
     pop bp
-    
     ret
