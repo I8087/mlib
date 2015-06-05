f1e908a21fe462d10821f84a43cc1ffa7eeac99e
I8087 <pythonic86@gmail.com>
2015-06-05 12:54:42 -0400

    Update os_load_file.asm
    Finished this function.

--- src/os_load_file.asm
+++ src/os_load_file.asm
@@ -1,1 +1,14 @@
-
+_os_load_file:
+    push bp
+    mov bp, sp
+    
+    mov ax, word [bp+4]
+    mov cx, word [bp+6]
+    
+    call os_load_file
+    
+    mov ax, bx
+    
+    pop bp
+    
+    ret
