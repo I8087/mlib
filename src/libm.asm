0c43e6c1f72b40bee2b8a207836c452da48ec888
I8087 <pythonic86@gmail.com>
2015-06-05 17:44:52 -0400

    Create libm.asm

---
+++ src/libm.asm
@@ -0,0 +1,14 @@
+%include "..\include\mikedev.inc"
+
+; Include the disk functions.
+%include "get_file_list.asm"
+%include "load_file.asm"
+%include "write_file.asm"
+%include "file_exists.asm"
+%include "create_file.asm"
+%include "remove_file.asm"
+%include "rename_file.asm"
+%include "get_file_size.asm"
+
+; Include the screen functions.
+%include "print_string.asm"
