8ca4d8e7d0742fe5a118579d9d0286c539ef5dfc
I8087 <pythonic86@gmail.com>
2015-06-05 17:03:40 -0400

    Update write_file.asm
    Created the function.

--- src/write_file.asm
+++ src/write_file.asm
@@ -1,1 +1,27 @@
+_write_file:
+    push bp
+    mov bp, sp
+    sub sp, 2
 
+    mov [ep-2], bx
+
+    mov ax, word [bp+4]
+    mov bx, word [bp+6]
+    mov cx, word [bp+8]
+
+    call os_write_file
+
+    jc .failed
+
+    xor ax, ax
+    jmp .skip
+
+.failed:
+    mov ax, 1
+
+.skip:
+    mov bx, word [ep-2]
+
+    mov sp, bp
+    pop bp
+    ret
