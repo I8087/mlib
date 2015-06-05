416a826ad3acf6e82f695aeb2c851a558fbd2c84
I8087 <pythonic86@gmail.com>
2015-06-05 16:54:15 -0400

    Update libm.h
    Changed the names of the functions to match them.

--- include/libm.h
+++ include/libm.h
@@ -1,19 +1,19 @@
-#ifndef NULL
-#define NULL (void*) 0
-#endif
-
 #ifndef _LIBM
 #define _LIBM
 
-// Define the os functions.
-char* os_get_file_list(char* buffer);
-char os_load_file(char* string, unsigned short location);
-char os_write_file(char* filename, char* buffer, unsigned short bytestowrite);
-char os_file_exists(char* filename);
-void os_create_file(char* filename);
-void os_remove_file(char* filename);
-void os_rename_file(char* old_filename, char* new_oldname);
-short os_get_file_size(char* filename);
+#ifndef NULL
+#define NULL ((void*) 0)
+#endif
+
+// Define the disk functions.
+char* get_file_list(char* buffer);
+char load_file(char* string, unsigned short location);
+char write_file(char* filename, char* buffer, unsigned short bytestowrite);
+char file_exists(char* filename);
+void create_file(char* filename);
+void remove_file(char* filename);
+void rename_file(char* old_filename, char* new_oldname);
+short get_file_size(char* filename);
 
 // Define the keyboard functions.
 
