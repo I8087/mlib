72576da4ab72b7436a3b09a09db67804eb16cdad
I8087 <pythonic86@gmail.com>
2015-06-05 17:04:21 -0400

    Update load_file.asm
    CX doesn't need to be saved.

--- src/load_file.asm
+++ src/load_file.asm
@@ -1,19 +1,13 @@
 _load_file:
     push bp
     mov bp, sp
-    sub sp, 2
-
-    mov word [ep-2], cx
 
     mov ax, word [bp+4]
     mov cx, word [bp+6]
 
     call os_load_file
 
-    mov [ep-2], word cx
-
     mov ax, bx
 
-    mov sp, bp
     pop bp
     ret
