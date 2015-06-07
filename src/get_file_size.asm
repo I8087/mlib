851855918dfcb2686cacd9abd301ecac80bca04a
I8087 <pythonic86@gmail.com>
2015-06-07 15:26:23 -0400

    Update get_file_size.asm
    Created the function.

--- src/get_file_size.asm
+++ src/get_file_size.asm
@@ -1,1 +1,25 @@
+global _get_file_size
 
+_get_file_size:
+    push bp
+    mov bp, sp
+    sub sp, 2
+
+    mov [bp-2], bx
+
+    mov ax, word [bp+4]
+
+    call os_get_file_size
+
+    jnc .skip
+
+    xor bx, bx
+
+.skip:
+    mov ax, bx
+
+    mov bx, word [bp-2]
+
+    mov sp, bp
+    pop bp
+    ret
