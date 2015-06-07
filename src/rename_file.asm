f970a8e582fecc433ba19be3df4b0e577fbb6f96
I8087 <pythonic86@gmail.com>
2015-06-07 15:19:17 -0400

    Update rename_file.asm
    Created the function.

--- src/rename_file.asm
+++ src/rename_file.asm
@@ -1,1 +1,19 @@
+global _rename_file
 
+_rename_file:
+    push bp
+    mov bp, sp
+    sub sp, 2
+    
+    mov [bp-2], bx
+
+    mov ax, word [bp+4]
+    mov bx, word [bp+6]
+
+    call os_rename_file
+
+    mov bx, word [bp-2]
+
+    mov sp, bp
+    pop bp
+    ret
