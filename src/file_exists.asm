0bdedb45ddab63f0617b44988a78e63b503becf4
I8087 <pythonic86@gmail.com>
2015-06-07 15:24:01 -0400

    Update file_exists.asm
    Created the function.

--- src/file_exists.asm
+++ src/file_exists.asm
@@ -1,1 +1,21 @@
+global _file_exists
 
+_file_exists:
+    push bp
+    mov bp, sp
+
+    mov ax, word [bp+4]
+
+    call os_file_exists
+
+    jc .failed
+
+    xor ax, ax
+    jmp .skip
+
+.failed:
+    mov ax, 1
+
+.skip:
+    pop bp
+    ret
