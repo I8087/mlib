d10bcca3860a136bc386a03c7e4e7cc37eb535f0
I8087 <pythonic86@gmail.com>
2015-06-07 15:20:48 -0400

    Update load_file.asm
    Preserved BX.

--- src/load_file.asm
+++ src/load_file.asm
@@ -3,6 +3,9 @@ global _load_file
 _load_file:
     push bp
     mov bp, sp
+    sub sp, 2
+
+    mov [bp-2], bx
 
     mov ax, word [bp+4]
     mov cx, word [bp+6]
@@ -11,5 +14,8 @@ _load_file:
 
     mov ax, bx
 
+    mov bx, word [bp-2]
+
+    mov sp, bp
     pop bp
     ret
