5d9c4a474dbf6badb6cccd2d88cf65fcfb7f3fa2
I8087 <pythonic86@gmail.com>
2015-06-11 16:49:22 -0400

    Update mlib.h
    Added all of the screen functions.

--- include/mlib.h
+++ include/mlib.h
@@ -34,5 +34,25 @@ short check_for_key(void);
 
 // Define the screen functions.
 void print_string(char* string);
+void clear_screen(void);
+void move_cursor(unsigned short cursor);
+short get_cursor_pos(void);
+void print_horiz_line(bool double_line);
+void show_cursor(void);
+void hide_cursor(void);
+void draw_block(unsigned short color, unsigned char start_x, unsigned char start_y, unsigned char end_x, unsigned char end_y);
+char* file_selector(void);
+char list_dialog(char* list, char* string1, char* string2);
+void draw_background(char* top, char* bottom, unsigned short color);
+void print_newline(void);
+void dump_registers(void);
+bool dialog_box(char* string1, char* string2, char* string3, bool mode);
+void print_space(void);
+void dump_string(char* string);
+void print_digit(unsigned short digit);
+void print_1hex(unsigned char digit);
+void print_2hex(unsigned char digit);
+void print_4hex(unsigned short digit);
+char* input_string(char* buffer);
 
 #endif
