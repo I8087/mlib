9ab280228bf6aee27b8550e5612ebbb1c3ab9a9d
I8087 <pythonic86@gmail.com>
2015-07-23 11:25:04 -0400

    Comment style change.
    Replaced C99 comments with ANSI C comments to conform to standards.

--- include/mlib.h
+++ include/mlib.h
@@ -1,7 +1,7 @@
 #ifndef MLIB_H
 #define MLIB_H
 
-// Define some helpful keywords.
+/* Define some helpful keywords. */
 #ifndef NULL
 #define NULL ((void*) 0)
 #endif
@@ -18,7 +18,7 @@ typedef int bool;
 #define false 0
 #endif
 
-// Define the disk functions.
+/* Define the disk functions. */
 char* get_file_list(char* buffer);
 bool load_file(char* string, unsigned short location);
 bool write_file(char* filename, char* buffer, unsigned short bytestowrite);
@@ -28,11 +28,11 @@ void remove_file(char* filename);
 void rename_file(char* old_filename, char* new_oldname);
 short get_file_size(char* filename);
 
-// Define the keyboard functions.
+/* Define the keyboard functions. */
 short wait_for_key(void);
 short check_for_key(void);
 
-// Define the screen functions.
+/* Define the screen functions. */
 void print_string(char* string);
 void clear_screen(void);
 void move_cursor(unsigned short cursor);
@@ -55,11 +55,11 @@ void print_2hex(unsigned char digit);
 void print_4hex(unsigned short digit);
 char* input_string(char* buffer);
 
-// Define the sound functions.
+/* Define the sound functions. */
 void speaker_tone(unsigned short frequency);
 void speaker_off(void);
 
-// Define the BASIC interpreter function.
+/* Define the BASIC interpreter function. */
 void run_basic(char* code, unsigned short size);
 
 #endif
