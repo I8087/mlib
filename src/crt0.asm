3770c28efbae6e98730fc6d66ccc7c106ad63439
I8087 <pythonic86@gmail.com>
2015-07-31 13:54:20 -0400

    Update crt0.asm
    Finished up some stuff.

--- src/crt0.asm
+++ src/crt0.asm
@@ -9,16 +9,29 @@ __start:
     cmp byte [si], 0x00
     je .null
 
-    push si
+    ; Move the only argv into **argv.
+    mov [.buffer], si
+
+    ; Setup the initial parameters.
+    push word .buffer
     push word 1
     jmp .skip
 
 .null:
+    ; If SI is null, then pass null values.
     push word 0
     push word 0
 .skip:
 
+    ; Call the main function and clean up after it.
     call _main
+    add sp, 4
+
+    ; NOTE: An os_exit function was submitted to MikeOS.
+    ; Hopefully it will be implemented in version 4.6
 
     pop bp
     ret
+
+; **argv
+.buffer: dw 0
