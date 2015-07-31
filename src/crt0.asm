54f7bed26e3ba0125098f4e22795eed74df407a8
I8087 <pythonic86@gmail.com>
2015-07-31 13:00:35 -0400

    Update crt0.asm
    Hopefully this is good enough for mlib.

--- src/crt0.asm
+++ src/crt0.asm
@@ -1,1 +1,23 @@
+global _start
 
+__start:
+    push bp
+    mov bp, sp
+
+    ; Setup argc and argv.
+    cmp byte [si], 0x00
+    je .null
+
+    push si
+    push word 1
+    jmp .skip
+
+.null:
+    push word 0
+    push word 0
+.skip:
+
+    call _main
+
+    pop bp
+    ret
