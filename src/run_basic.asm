5719047e6a139e280af2e506774a409f0d2653bc
ZeroKelvinKeyboard <zerokelvinkeyboard@gmail.com>
2016-06-21 17:38:42 +0930

    Fixed calling convention in run_basic.

--- src/run_basic.asm
+++ src/run_basic.asm
@@ -3,9 +3,10 @@ global _run_basic
 _run_basic:
     push bp
     mov bp, sp
-    sub sp, 2
+    sub sp, 4
 
     mov [bp-2], bx
+    mov [bp-4], si
 
     mov ax, word [bp+4]
     mov bx, word [bp+6]
@@ -13,6 +14,7 @@ _run_basic:
 
     call os_run_basic
 
+    mov si, word [bp-4]
     mov bx, word [bp-2]
 
     mov sp, bp
