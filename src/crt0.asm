e28a6bd04d698ac2fdb149eb69f51e6a599c79c0
I8087 <i8086@protonmail.com>
2016-07-12 11:38:37 -0400

    Fixed #6
    Fixed the args.

--- src/crt0.asm
+++ src/crt0.asm
@@ -5,28 +5,67 @@ __start:
     push bp
     mov bp, sp
 
-    ; Setup argc and argv.
-    cmp byte [si], 0x00
-    je .null
+    ; argv
+    mov bx, .buffer
 
-    ; Move the only argv into **argv.
-    mov [.buffer], si
+    ; argv[0] is always the file's name!
+    mov [bx], 0
+    add bx, 2
 
-    ; Setup the initial parameters.
+    ; argc
+    mov cx, 1
+
+    ; If si is 0, then there are no args.
+    cmp si, 0
+    je .done
+
+    ; Decrease because of the upcoming loop.
+    dec si
+
+.loop:
+    inc si
+
+    ; Skip multiple blank spaces!
+    cmp byte [si], ' '
+    je .loop
+
+    ; Found an arg.
+    mov [bx], si
+    inc cx
+    add bx, 2
+
+    ; Make sure argv doesn't overflow!
+    cmp cx, 12
+    je .arg_error
+
+.find:
+    ; That's all the args.
+    cmp byte [si], 0
+    je .done
+
+    ; Go to the next arg!
+    cmp byte [si], ' '
+    je .next
+
+    inc si
+
+    jmp .find
+
+.next:
+    mov byte [si], 0
+
+    jmp .loop
+
+.done:
+    ; Push argv and argc respectively.
     push word .buffer
-    push word 1
-    jmp .skip
-
-.null:
-    ; If SI is null, then pass null values.
-    push word 0
-    push word 0
-.skip:
+    push cx
 
     ; Call the main function and clean up after it.
     call _main
     add sp, 4
 
+.exit:
     ; NOTE: An os_exit function was submitted to MikeOS.
     ; Hopefully it will be implemented in version 4.6
 
@@ -34,5 +73,10 @@ __start:
     pop bp
     ret
 
-; **argv
-.buffer: dw 0
+.arg_error:
+    push .arg_str
+    call _print_string
+    jmp .exit
+
+.arg_str: db "Too many arguments!", 13, 10, 0
+.buffer: times 11 dw 0
