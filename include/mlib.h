ecde93718c57b349bd3871ca5730dcda71bc256a
I8087 <i8086@protonmail.com>
2019-06-09 21:58:56 -0700

    Fixed #9
    Added Mikedev definitions.

--- include/mlib.h
+++ include/mlib.h
@@ -3,7 +3,7 @@
 
 /* Define some helpful keywords. */
 #ifndef NULL
-#define NULL ((void*) 0)
+#define NULL ((void*) 0x0)
 #endif
 
 #ifndef bool
@@ -11,11 +11,56 @@ typedef int bool;
 #endif
 
 #ifndef true
-#define true 1
+#define true 0x1
 #endif
 
 #ifndef false
-#define false 0
+#define false 0x0
+#endif
+
+/* Define Mikedev definitions. */
+#ifndef BLACK_ON_WHITE
+#define BLACK_ON_WHITE 0xF0
+#endif
+
+#ifndef WHITE_ON_BLACK
+#define WHITE_ON_BLACK 0x0F
+#endif
+
+#ifndef WHITE_ON_LIGHT_RED
+#define WHITE_ON_LIGHT_RED 0xCF
+#endif
+
+#ifndef WHITE_ON_GREEN
+#define WHITE_ON_GREEN 0x2F
+#endif
+
+#ifndef RED_ON_LIGHT_GREEN
+#define WHITE_ON_LIGHT_RED 0xA4
+#endif
+
+#ifndef KEY_UP
+#define KEY_UP 0x48
+#endif
+
+#ifndef KEY_DOWN
+#define KEY_DOWN 0x50
+#endif
+
+#ifndef KEY_LEFT
+#define KEY_LEFT 0x4B
+#endif
+
+#ifndef KEY_RIGHT
+#define KEY_RIGHT 0x4D
+#endif
+
+#ifndef KEY_ESC
+#define KEY_ESC 0x1B
+#endif
+
+#ifndef KEY_ENTER
+#define KEY_ENTER 0xD
 #endif
 
 /* A global variable for I/O error reporting. */
